19d458cfc9939e8e2424673d5c1488c17de2c5f7
Rafael Brito <rafael.souzabrito7@gmail.com>
2020-08-10 21:20:53 -0300

    challenge completed

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tp1_rafaelbrito.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
